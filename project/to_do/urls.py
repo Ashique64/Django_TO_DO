6cd9b6dc1af6fc415e4b5c83a4a4e0b7f8ab737f
Ashique <ashique9496@gmail.com>
2024-08-19 17:08:08 +0530

    Completed

--- project/to_do/urls.py
+++ project/to_do/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('add/',views.adding,name='add'),
+    path('delete/<int:id>',views.deleting,name='delete'),
+    path('edit/<int:id>',views.edit,name='edit'),
     
 ]
--- project/to_do/views.py
+++ project/to_do/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import ToDoItems
 
 # Create your views here.
 
 
 def home(request):
-    return render(request,'main.html')
+    todos = ToDoItems.objects.all()
+    return render(request,'main.html', {'todos':todos})
+
+def adding(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        ToDoItems.objects.create(title=title)
+        return redirect('home')
+
+def deleting(request,id):
+    todo = ToDoItems.objects.get(id=id)
+    todo.delete()
+    return redirect('home')
+
+def edit(request,id):
+    
+    todo = ToDoItems.objects.get(id=id)
+    
+    if request.method == 'POST':
+        new_title = request.POST.get('title')
+        todo.title = new_title
+        todo.save()
+        return redirect('home')
+    
+    return render(request,'edit.html',{'todo':todo})
